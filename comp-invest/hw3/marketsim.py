4d8af6d9108601cebb46c9eaac4b7ab72d8d2810
Denis Kutukov <mail@dskut.ru>
2013-10-03 23:50:08 +0400

    marketsim.py done

--- comp-invest/hw3/marketsim.py
+++ comp-invest/hw3/marketsim.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
 import sys
+import os
+import operator
 import pandas as pd
 import numpy as np
 import math
@@ -18,6 +20,9 @@ def order_comparer(first, second):
         return cmp(first['month'], second['month'])
     return cmp(first['day'], second['day'])
 
+def get_order_datetime(order, hour=16):
+    return dt.datetime(order['year'], order['month'], order['day'], hour)
+
 def read_orders(orders_path):
     orders_file = open(orders_path)
     res = []
@@ -27,15 +32,13 @@ def read_orders(orders_path):
         order['year'] = int(data[0])
         order['month'] = int(data[1])
         order['day'] = int(data[2])
+        order['datetime'] = get_order_datetime(order)
         order['symbol'] = data[3]
         order['action'] = data[4].lower()
         order['shares'] = int(data[5])
         res.append(order)
     res.sort(cmp = order_comparer)
     return res
-
-def get_order_datetime(order):
-    return dt.datetime(order['year'], order['month'], order['day'], 16)
 
 def get_symbols(orders):
     return list(set([order['symbol'] for order in orders]))
@@ -46,37 +49,72 @@ def read_prices(dt_start, dt_end, symbols):
     ls_keys = ['open', 'high', 'low', 'close', 'volume', 'actual_close']
     ldf_data = dataobj.get_data(ldt_timestamps, symbols, ls_keys)
     d_data = dict(zip(ls_keys, ldf_data))
-    prices = d_data['actual_close'].values
+    prices = d_data['close'].values
     sym2prices = []
     for price in prices:
         sym2prices.append(dict(zip(symbols, price)))
     res = dict(zip(ldt_timestamps, sym2prices))
     return res
 
-def execute(cash, order, prices):
-    dt = get_order_datetime(order)
+def group_by_day(orders):
+    date2orders = {} 
+    for order in orders:
+        dt = order["datetime"]
+        if dt not in date2orders:
+            date2orders[dt] = []
+        date2orders[dt].append(order)
+    return date2orders
+
+class DailyOrders:
+    def __init__(self, date, orders):
+        self.date = date
+        self.orders = orders
+
+    def __repr__(self):
+        return str(self.date) + " => " + str(self.orders) + "\n"
+
+def flatten(date2orders):
+    res = []
+    for date, orders in date2orders.iteritems():
+        dailyOrders = DailyOrders(date, orders)
+        res.append(dailyOrders)
+    res.sort(key = operator.attrgetter("date"))
+    return res
+
+def execute_order(order, prices):
+    dt = order["datetime"]
     price = float(prices[dt][order['symbol']])
     val = price * order['shares']
     if order['action'] == 'buy':
-        return cash - val
+        return -val
     else:
-        return cash + val
+        return val
+
+def execute(daily_orders, prices):
+    res = 0
+    for order in daily_orders.orders:
+        res += execute_order(order, prices)
+    return res
 
 def main():
     if len(sys.argv) < 4:
         print("usage: " + sys.argv[0] + " <cash> <orders.csv> <values.csv>")
         exit(1)
 
-    cash = int(sys.argv[1])
+    cash = float(sys.argv[1])
     orders_path = sys.argv[2]
     values_path = sys.argv[3]
 
     orders = read_orders(orders_path)
     prices = read_prices(get_order_datetime(orders[0]), get_order_datetime(orders[-1]), get_symbols(orders))
+    orders_grouped = group_by_day(orders)
+    orders_by_date_list = flatten(orders_grouped)
     
-    print cash
-    for order in orders:
-        cash = execute(cash, order, prices)
-        print order['year'], order['month'], order['day'], cash 
+    out_file = open(values_path, 'w')
+    for daily_orders in orders_by_date_list:
+        cash += execute(daily_orders, prices)
+        dt = daily_orders.date
+        s = "{0}, {1}, {2}, {3}".format(dt.year, dt.month, dt.day, cash)
+        out_file.write(s + os.linesep)
 
 main()
